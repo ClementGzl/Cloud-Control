512f2068b1edcc89e7782e79f07c823f20fb0fca
Clement Gonzalvez <32064592+ClemrentGzl@users.noreply.github.com>
2018-08-16 17:52:48 +0100

    Just a test

---
+++ Cloud Control/Data model/Instance.swift
@@ -0,0 +1,19 @@
+//
+//  Instance.swift
+//  Cloud Control
+//
+//  Created by Clément Gonzalvez on 15/08/2018.
+//  Copyright © 2018 Clément. All rights reserved.
+//
+
+import Foundation
+
+class Instance {
+    
+    var region: String = ""
+    var id: String = ""
+    var name: String = ""
+    var status: String = ""
+    var launchTime: String = ""
+    
+}
