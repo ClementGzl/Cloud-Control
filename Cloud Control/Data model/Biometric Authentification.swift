262fecb3a92983f9afc7415c8605f7eec7a16f76
Clément Gonzalvez <clement@gonzalvez.com>
2018-11-12 13:44:04 +0000

    Update the biometric authentification functionality and clean the code.

---
+++ Cloud Control/Data model/Biometric Authentification.swift
@@ -0,0 +1,38 @@
+//
+//  Biometric Authentification.swift
+//  Cloud Control
+//
+//  Created by Clément Gonzalvez on 12/11/2018.
+//  Copyright © 2018 Clément. All rights reserved.
+//
+
+import Foundation
+import LocalAuthentication
+
+class BiometricIDAuth {
+    
+    let context = LAContext()
+    
+    func canEvaluatePolicy() -> Bool {
+        return context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+    }
+    
+    func biometricType() -> BiometricType {
+        let _ = context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+        switch context.biometryType {
+        case .none:
+            return .none
+        case .touchID:
+            return .touchID
+        case .faceID:
+            return .faceID
+        }
+    }
+    
+    enum BiometricType {
+        case none
+        case touchID
+        case faceID
+    }
+    
+}
--- Cloud Control/Views/Instance Cell/InstanceCell.swift
+++ Cloud Control/Views/Instance Cell/InstanceCell.swift
@@ -46,6 +46,7 @@ class InstanceCell: UITableViewCell {
     weak var delegate: InstanceCellDelegate?
     
     override func setSelected(_ selected: Bool, animated: Bool) {
+        
         super.setSelected(selected, animated: animated)
 
     }
