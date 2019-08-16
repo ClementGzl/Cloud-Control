609669231e0d245bad77c1ca395a6144ec829980
Clement Gonzalvez <32064592+ClemrentGzl@users.noreply.github.com>
2019-08-16 23:18:05 +0100

    Adding Core Data to the project. Conforming the new Region object to it. Create the RegionFetcher class to fetch the array of regions.

--- Cloud Control/Controllers/TableViewController.swift
+++ Cloud Control/Controllers/TableViewController.swift
@@ -12,26 +12,32 @@ import SwiftyJSON
 import SVProgressHUD
 import UserNotifications
 
+protocol RegionFilterDelegate: class {
+    var selectedRegions: [Region] {get}
+}
+
 class TableViewController: UITableViewController {
 
-    var instancesArray = [Instance]()
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Regions.plist")
-    
+    var instances = [Instance]()
+
     let defaults = UserDefaults.standard
     
     let instanceURL = secretInstanceURL
     let listURL = secretListURL
     let headers: HTTPHeaders = ["x-api-key": secretApiKey]
     var refresher: UIRefreshControl!
-    var id : String = ""
+    var regions: [Region] = []
+    
+    weak var delegate: RegionFilterDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView = UITableView(frame: .zero, style: .grouped)
+        
         UIApplication.shared.applicationIconBadgeNumber = 0
 
-        tableView.register(UINib(nibName: "instanceCell", bundle: nil), forCellReuseIdentifier: "customInstanceCell")
+        tableView.register(UINib(nibName: "InstanceCell", bundle: nil), forCellReuseIdentifier: "InstanceCell")
         loadRegions()
 
         SVProgressHUD.setDefaultMaskType(.clear)
@@ -40,15 +46,16 @@ class TableViewController: UITableViewController {
         refresher.tintColor = .white
         refresher.addTarget(self, action: #selector(TableViewController.pullRefreshStatus), for: UIControl.Event.valueChanged)
         tableView.refreshControl = refresher
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
         
+        let fetcher = RegionFetcher()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        
         showSVProgressHUD()
         loadRegions()
         getStatus(url: listURL)
-
     }
     
     @IBAction func goToinfo(_ sender: Any) {
@@ -58,15 +65,15 @@ class TableViewController: UITableViewController {
     //MARK: - Tableview data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return instancesArray.count
+        return instances.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "instanceCell", for: indexPath) as! InstanceCell
-        cell.nameLabel.text = instancesArray[indexPath.row].name
-        cell.statusLabel.text = instancesArray[indexPath.row].status
-        cell.launchTimeLabel.text = instancesArray[indexPath.row].launchTime
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InstanceCell", for: indexPath) as! InstanceCell
+        cell.nameLabel.text = instances[indexPath.row].name
+        cell.statusLabel.text = instances[indexPath.row].status
+        cell.launchTimeLabel.text = instances[indexPath.row].launchTime
 
         cell.delegate = self
 
@@ -76,9 +83,7 @@ class TableViewController: UITableViewController {
     //MARK: - Networking
     
     @objc func pullRefreshStatus() {
-        
         self.getStatus(url: listURL)
-        
     }
     
     func getInstancesList(url: String) {
@@ -91,29 +96,22 @@ class TableViewController: UITableViewController {
                 print("Success, got the instances list")
                 
                 let instancesListJSON = JSON(response.result.value!)
-                
-                print(instancesListJSON)
 
                 SVProgressHUD.dismiss()
                 
             } else {
                 print("Error getting instances list, \(response.result.error!)")
                 
-                    SVProgressHUD.dismiss()
-                
+                SVProgressHUD.dismiss()
             }
-            
         }
-        
     }
     
     @objc func getStatus(url : String) {
         
-        let selectedRegions = regionsArray.filter { $0.isSelected == true }.map {$0.region}.joined(separator: ",")
+        let selectedRegions = delegate?.selectedRegions.map({$0.rawRegion ?? ""}).joined(separator: ",")
 
-        let params = ["regions" : selectedRegions]
-        
-        print("I am here")
+        let params: Parameters = ["regions" : selectedRegions ?? ""]
         
         Alamofire.request(url, method: .get, parameters: params, headers: headers).responseJSON {
             response in
@@ -129,18 +127,13 @@ class TableViewController: UITableViewController {
                 SVProgressHUD.dismiss()
                 self.refresher.endRefreshing()
                 
-            }
-
-            else {
+            } else {
                 print("Error getting status, \(response.result.error!)")
                 
                 SVProgressHUD.dismiss()
                 self.refresher.endRefreshing()
-
             }
-    
         }
-        
     }
     
     func actionInstance(url: String, region: String, id: String, action : String) {
@@ -188,7 +181,7 @@ class TableViewController: UITableViewController {
     
     func updateInstancesArray(json: JSON) {
         
-        instancesArray.removeAll()
+        instances.removeAll()
 
         for (_, subJson):(String, JSON) in json {
             
@@ -217,7 +210,7 @@ class TableViewController: UITableViewController {
                 temporaryInstance.status = formatStatusCode(code: statusCode)
                 
                 if temporaryInstance.status != "Terminated" {
-                    instancesArray.append(temporaryInstance)
+                    instances.append(temporaryInstance)
                 }
                 
                 print("Success updating instances array")
@@ -225,9 +218,7 @@ class TableViewController: UITableViewController {
             } else {
                 print("Error updating instances array")
             }
-            
         }
-            
     }
     
     func formatDate(date: String) -> String {
@@ -235,52 +226,47 @@ class TableViewController: UITableViewController {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
         
-        guard let dateFromString = dateFormatter.date(from: date) else {return "Unable to format the date"}
+        guard let dateFromString = dateFormatter.date(from: date) else {
+            return "Unable to format the date"
+        }
         
         dateFormatter.dateFormat = "dd/MM/yyyy' at 'HH:mm"
         
         return dateFormatter.string(from: dateFromString)
-        
     }
     
     func formatStatusCode(code: Int) -> String {
-        
-        var stringStatus = ""
-        
+
         switch code {
         case 0 :
-            stringStatus = "Pending"
+            return "Pending"
         case 16 :
-            stringStatus = "Running"
+            return "Running"
         case 32 :
-            stringStatus = "Shutting down"
+            return "Shutting down"
         case 48 :
-            stringStatus = "Terminated"
+            return "Terminated"
         case 64 :
-            stringStatus = "Stopping"
+            return "Stopping"
         case 80 :
-            stringStatus = "Stopped"
+            return "Stopped"
         default:
-            stringStatus = "Error getting status"
+            return "Error getting status"
         }
-        
-        return stringStatus
-        
     }
     
     // UI Update
     
     func loadRegions() {
         
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                regionsArray = try decoder.decode([Region].self, from: data)
-            } catch {
-                print("Error decoding regionsArray \(error)")
-            }
-        }
-        
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder()
+//            do {
+//                regionsArray = try decoder.decode([Region].self, from: data)
+//            } catch {
+//                print("Error decoding regionsArray \(error)")
+//            }
+//        }
     }
     
     func showSVProgressHUD() {
@@ -288,7 +274,6 @@ class TableViewController: UITableViewController {
             SVProgressHUD.show()
         }
     }
-        
 }
 
 extension TableViewController: InstanceCellDelegate {
@@ -299,22 +284,22 @@ extension TableViewController: InstanceCellDelegate {
         
             if cell.switchButton.isOn == true {
                 
-                instancesArray[indexPath.row].status = "Pending"
-                self.actionInstance(url: instanceURL, region: instancesArray[indexPath.row].region, id: instancesArray[indexPath.row].id, action: "start")
+                instances[indexPath.row].status = "Pending"
+                self.actionInstance(url: instanceURL, region: instances[indexPath.row].region, id: instances[indexPath.row].id, action: "start")
                 
                 if defaults.bool(forKey: "notificationsSetting") {
-                    createNotification(instance: self.instancesArray[indexPath.row])
-                    print("Notification for instance \(self.instancesArray[indexPath.row].name) successfully added")
+                    createNotification(instance: self.instances[indexPath.row])
+                    print("Notification for instance \(self.instances[indexPath.row].name) successfully added")
                 }
                 
             } else if cell.switchButton.isOn == false {
                 
-                instancesArray[indexPath.row].status = "Stopping"
-                self.actionInstance(url: instanceURL, region: instancesArray[indexPath.row].region, id: instancesArray[indexPath.row].id, action: "stop")
+                instances[indexPath.row].status = "Stopping"
+                self.actionInstance(url: instanceURL, region: instances[indexPath.row].region, id: instances[indexPath.row].id, action: "stop")
                 
                 if defaults.bool(forKey: "notificationsSetting") {
-                    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [self.instancesArray[indexPath.row].name])
-                    print("Notification for instance \(self.instancesArray[indexPath.row].name) successfully removed")
+                    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [self.instances[indexPath.row].name])
+                    print("Notification for instance \(self.instances[indexPath.row].name) successfully removed")
                 }
             }
         }
@@ -329,8 +314,6 @@ extension TableViewController: UNUserNotificationCenterDelegate {
         
         let content = UNMutableNotificationContent()
         UNUserNotificationCenter.current().delegate = self
-        
-//        let userInfo: [String : Any] = ["Instance": instance]
 
         content.title = "Your instance \(instance.name) is still running"
         content.body = "You may want to turn it off"
@@ -375,20 +358,6 @@ extension TableViewController: UNUserNotificationCenterDelegate {
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-
         completionHandler([UNNotificationPresentationOptions.alert, UNNotificationPresentationOptions.badge, UNNotificationPresentationOptions.sound])
     }
-}
-struct Notification {
-    
-    struct Category {
-        static let tutorial = "tutorial"
-    }
-    
-    struct Action {
-        static let snooze = "snoozeInstance"
-        static let showDetails = "showDetails"
-        static let unsubscribe = "unsubscribe"
-    }
-    
 }
--- Cloud Control/Data model/Region.swift
+++ Cloud Control/Data model/Region.swift
@@ -7,36 +7,138 @@
 //
 
 import UIKit
+import CoreData
 
-class Region: Codable {
+class RegionFetcher {
     
-    var name: String = ""
-    var region: String = ""
-    var isSelected: Bool = false
+    var regions: [Region] = []
     
-    init(name forName : String, region forRegion : String) {
-        name = forName
-        region = forRegion
+    static let sharedInstance = RegionFetcher()
+    
+    init() {
+        fetch()
+    }
+    
+    private func fetch() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<Region>(entityName: "Region")
+        
+        do {
+            let regions = try managedContext.fetch(fetchRequest)
+            
+            if regions.isEmpty {
+                generateInitialRegions()
+                fetch()
+                return
+            } else {
+                self.regions = regions
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func save() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        do {
+            try managedContext.save()
+        } catch {
+            print("Error saving regions: \(error)")
+        }
+        
+    }
+    
+    private func generateInitialRegions() {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        guard let userEntity = NSEntityDescription.entity(forEntityName: "Region", in: managedContext) else {
+            return
+        }
+        
+        let usEast1 = Region(entity: userEntity, insertInto: managedContext)
+        usEast1.rawRegion = "us-East-1"
+        usEast1.friendlyName = "US East (N. Virginia)"
+        
+        let usEast2 = Region(entity: userEntity, insertInto: managedContext)
+        usEast2.rawRegion = "us-East-2"
+        usEast2.friendlyName = "US East (Ohio)"
+        
+        let usWest1 = Region(entity: userEntity, insertInto: managedContext)
+        usWest1.rawRegion = "us-West-1"
+        usWest1.friendlyName = "US West (N. California)"
+        
+        let usWest2 = Region(entity: userEntity, insertInto: managedContext)
+        usWest2.rawRegion = "us-West-2"
+        usWest2.friendlyName = "US West (Oregon)"
+        
+        let apNorthEast1 = Region(entity: userEntity, insertInto: managedContext)
+        apNorthEast1.rawRegion = "ap-NorthEast-1"
+        apNorthEast1.friendlyName = "Asia Pacific (Tokyo)"
+        
+        let apNorthEast2 = Region(entity: userEntity, insertInto: managedContext)
+        apNorthEast2.rawRegion = "ap-NorthEast-2"
+        apNorthEast2.friendlyName = "Asia Pacific (Seoul)"
+        
+        let apSouth1 = Region(entity: userEntity, insertInto: managedContext)
+        apSouth1.rawRegion = "ap-South-1"
+        apSouth1.friendlyName = "Asia Pacific (Mumbai)"
+        
+        let apSouthEast1 = Region(entity: userEntity, insertInto: managedContext)
+        apSouthEast1.rawRegion = "ap-SouthEast-1"
+        apSouthEast1.friendlyName = "Asia Pacific (Singapore)"
+        
+        let apSouthEast2 = Region(entity: userEntity, insertInto: managedContext)
+        apSouthEast2.rawRegion = "ap-SouthEast-2"
+        apSouthEast2.friendlyName = "Asia Pacific (Sydney)"
+        
+        let caCentral1 = Region(entity: userEntity, insertInto: managedContext)
+        caCentral1.rawRegion = "ca-Central-1"
+        caCentral1.friendlyName = "Canada (Central)"
+        
+        let euCentral1 = Region(entity: userEntity, insertInto: managedContext)
+        euCentral1.rawRegion = "eu-Central-1"
+        euCentral1.friendlyName = "EU (Frankfurt)"
+        
+        let euWest1 = Region(entity: userEntity, insertInto: managedContext)
+        euWest1.rawRegion = "eu-West-1"
+        euWest1.friendlyName = "EU (Ireland)"
+        
+        let euWest2 = Region(entity: userEntity, insertInto: managedContext)
+        euWest2.rawRegion = "eu-West-2"
+        euWest2.friendlyName = "EU (London)"
+        
+        let euWest3 = Region(entity: userEntity, insertInto: managedContext)
+        euWest3.rawRegion = "eu-West-3"
+        euWest3.friendlyName = "EU (Paris)"
+        
+        let eunorth1 = Region(entity: userEntity, insertInto: managedContext)
+        eunorth1.rawRegion = "eu-north-1"
+        eunorth1.friendlyName = "EU (Stockholm)"
+        
+        let saEast1 = Region(entity: userEntity, insertInto: managedContext)
+        saEast1.rawRegion = "sa-East-1"
+        saEast1.friendlyName = "South America (São Paulo)"
+        
+        do {
+            try managedContext.save()
+        } catch {
+            print("Error saving inital regions: \(error)")
+        }
     }
     
 }
-
-    var regionsArray : [Region] = [
-        
-        Region(name:"US East (Ohio)", region:"us-east-2"),
-        Region(name: "US East (N. Virginia)", region: "us-east-1"),
-        Region(name: "US West (N. California)", region: "us-west-1"),
-        Region(name: "US West (Oregon)", region: "us-west-2"),
-        Region(name: "Asia Pacific (Tokyo)", region: "ap-northeast-1"),
-        Region(name: "Asia Pacific (Seoul)", region: "ap-northeast-2"),
-        Region(name: "Asia Pacific (Mumbai)", region: "ap-south-1"),
-        Region(name: "Asia Pacific (Singapore)", region: "ap-southeast-1"),
-        Region(name: "Asia Pacific (Sydney)", region: "ap-southeast-2"),
-        Region(name: "Canada (Central)", region: "ca-central-1"),
-        Region(name: "EU (Frankfurt)", region: "eu-central-1"),
-        Region(name: "EU (Ireland)", region: "eu-west-1"),
-        Region(name: "EU (London)", region: "eu-west-2"),
-        Region(name: "EU (Paris)", region: "eu-west-3"),
-        Region(name: "South America (São Paulo)", region: "sa-east-1")
-        
-    ]
